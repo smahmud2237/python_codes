0c73ae4d41d6b9b04358ecaf70c7404206a7eda7
smahmud2237 <800kingdom@gmail.com>
2026-07-24 22:43:32 +0600

    Numbers in Python

---
+++ ZERO_TO_HERO_BOOTCAMP/Obj_And_Data_Structure/numbers.py
@@ -0,0 +1,11 @@
+'''
+Numbers stores numerical information and come in two diff forms:
+1. Integers => Whole Numbers
+2. Floating Point - Numbers with a decimal
+
+'''
+a = 2     # integer
+b = 1.25   # float
+c = a + b
+print(c)
+
