1915cea2f2e2cfc5493d12ef19418ecb40216d8e
smahmud2237 <800kingdom@gmail.com>
2026-07-28 01:22:13 +0600

    Sets info added

---
+++ ZERO_TO_HERO_BOOTCAMP/Obj_And_Data_Structure/sets.py
@@ -0,0 +1,13 @@
+'''
+1. Sets are unordered collections of unique elements. Meaning there can only be one representative of the same object.
+Let's see some examples.
+'''
+myset = set()
+print(myset)
+# we can update set
+myset.add(1)
+print(myset)
+myset.add(2)
+print(myset)
+
+
