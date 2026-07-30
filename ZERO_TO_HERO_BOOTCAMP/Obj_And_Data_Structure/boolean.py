371c488a5516a92ed41e84348834a19a6df16251
smahmud2237 <800kingdom@gmail.com>
2026-07-30 12:00:44 +0600

    Add Boolean info

---
+++ ZERO_TO_HERO_BOOTCAMP/Obj_And_Data_Structure/boolean.py
@@ -0,0 +1,10 @@
+'''
+1. Booleans are operators that allow you to convey True or False statements.
+2. These are very important later on when we deal with control flow and logic!
+'''
+print(type(False))
+print(type(True))
+# some examples of boolean
+print(1>2)
+print(1==1)
+
