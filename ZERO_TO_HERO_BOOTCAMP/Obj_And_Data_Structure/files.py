785ae55aa01df690fdabe87b5f78e6574eff89ba
smahmud2237 <800kingdom@gmail.com>
2026-08-05 00:22:43 +0600

    Files info added

---
+++ ZERO_TO_HERO_BOOTCAMP/Obj_And_Data_Structure/files.py
@@ -0,0 +1,23 @@
+'''
+1. Let's go over how to perform simple I/O with basic.txt files.
+2. Also disscuss about file paths on computer. Let's get started!
+# write the below lines in jupiter nodebook and create a txt file:
+'%%writefile myfile.txt
+Hello this is a text File
+This is Second line 
+This is Third line'
+
+pwd
+myfile.open('fileName')
+myfile.read()
+myfile.close()
+
+# Reading, Writing, Appending Modes
+mode 'r' is for read only
+mode 'w' is for write only(will overwrite files or create new!)
+mode 'a' is for append only(will add on to files)
+mode 'r+' is reading and writing
+mode 'w+' is for writing and reading(Overwrites existing files or creates a new file!)
+'''
+
+
