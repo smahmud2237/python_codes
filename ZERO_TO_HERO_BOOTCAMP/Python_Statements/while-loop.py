48548791bffdb56d97770b08d560fd9ca2ff837e
smahmud2237 <800kingdom@gmail.com>
2026-08-11 00:05:56 +0600

    While loop info

---
+++ ZERO_TO_HERO_BOOTCAMP/Python_Statements/while-loop.py
@@ -0,0 +1,18 @@
+'''
+While loops will continue to execute a block of code while some condition remains True. For example, while
+my pool is not full, keep filling my pool with water. Or, while my dogs are still hungry, keep feeding my dogs.
+'''
+x = 0
+while x < 5:
+    print(f"The current value of x is {x}")
+    x += 1
+else:
+    print(f"X = {x}, is not less than 5")
+#---------------------------=========================---------------------------#
+
+
+
+
+
+
+
