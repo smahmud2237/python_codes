360e439fb25361c48357a66cf4ecf9c3146a77a2
smahmud2237 <800kingdom@gmail.com>
2026-08-14 08:10:40 +0600

    Comparison info

---
+++ ZERO_TO_HERO_BOOTCAMP/Comparison_Operators/comparison.py
@@ -0,0 +1,29 @@
+'''
+1. If the values of two operands are equal, then the condition becomes true. If not equal then false.
+Exp: a == a => true
+     a == b => false
+2. If the values of two operands are not equal, then codition becomes true. If equal then false.
+Exp: a != b => true
+     a != a => false
+3. If the value of left operand is greater than the value of right operand and vise-versa, then condition becomes true. 
+Exp: a > b [a=4, b=3] => true    
+     a < b [a=4, b=5] => true
+4. If the value of left operand is greater than or equal to the value of right operand, then condition becomes true.
+Exp: a >= b [a=4, b=3] => true    
+     a >= b [a=4, b=4] => true
+5. If the value of left operand is less than or equal to the value of right operand, then condition becomes true.
+Exp: a <= b [a=2, b=4] => true
+     a <= b [a=3, b=3] => true
+'''
+print(2==2)
+print(2!=2)
+print(2==3)
+print('Hello'=='Bye')
+print('Hey' == 'Hey')
+print(3 >= 2)
+print(4 <= 4)
+
+
+
+
+
