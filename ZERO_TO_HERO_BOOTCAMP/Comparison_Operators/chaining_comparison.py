221655244017dcc1810afaeef3a7db030e2d47da
smahmud2237 <800kingdom@gmail.com>
2026-08-15 17:27:00 +0600

    Chainig comparison info

---
+++ ZERO_TO_HERO_BOOTCAMP/Comparison_Operators/chaining_comparison.py
@@ -0,0 +1,14 @@
+'''
+1. We can use logical operators to combine comparisons: and, or, not
+Let's explore how to use it...
+'''
+print(('Hello' == 'Hello') and (2 == 2))
+print(('Hi' == 'Bye') and (3 == 3))
+print((100 == 100) or (2 != 2))
+print((100 == 50) or (2 != 3))
+print((10 == 100) or (2 == 2))
+
+# Now using not for boolean variable
+print(not(1 == 1)) 
+# or, 
+print(1 != 1)
