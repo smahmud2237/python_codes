3705f24ff1c298c9b2c424f8f27fe5c412b18e61
smahmud2237 <800kingdom@gmail.com>
2026-08-18 15:16:39 +0600

    Methods documentations

---
+++ ZERO_TO_HERO_BOOTCAMP/Methods_Fucntions/methods_docu.py
@@ -0,0 +1,14 @@
+'''
+Built-in objects in Python have a variety of methods we can use!
+Let's explore in a bit more details how to find methods and how to get information about them.
+print(help(varName.methodName)) ==> This is how we can see methods details information in our output screen.
+'''
+mylist = [1, 2, 3]
+# append 4 in the list
+mylist.append(4)
+print(mylist)
+# remove 4 from the list
+mylist.pop()
+print(mylist)
+#---------------------------=============>O<============---------------------------#
+
